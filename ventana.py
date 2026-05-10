a3cd8bf9e3f3ea11f91c96002f37356dd18f2a9b
Your Name <alejandroescandonescobar0@gmail.com>
2026-05-09 19:11:22 -0500

    se dibuja tabla

--- ventana.py
+++ ventana.py
@@ -2,34 +2,24 @@
 
 from PyQt5.QtWidgets import (
     QWidget,
-    QLabel,
     QVBoxLayout
 )
 
-from PyQt5.QtCore import Qt
+from tabla.dibujotabla import DibujoTabla
 
 
 class VentanaPrincipal(QWidget):
+
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Mi Ventana PyQt5")
+        self.setWindowTitle("Sistema")
         self.setGeometry(100, 100, 1000, 1000)
 
-        self.inicializar_ui()
+        self.layout = QVBoxLayout()
 
-    def inicializar_ui(self):
+        self.tabla = DibujoTabla()
 
-        layout = QVBoxLayout()
+        self.layout.addWidget(self.tabla)
 
-        titulo = QLabel("Ventana 1000 x 1000")
-        titulo.setAlignment(Qt.AlignCenter)
-
-        titulo.setStyleSheet("""
-            font-size: 28px;
-            font-weight: bold;
-        """)
-
-        layout.addWidget(titulo)
-
-        self.setLayout(layout)
+        self.setLayout(self.layout)
